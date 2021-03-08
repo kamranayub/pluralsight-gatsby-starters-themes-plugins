9a4321efa7052f88133d48161c6b5e5a8d60227c
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-08 14:09:18 -0600

    Add some terms

--- glossary-api/netlify/functions/glossary.js
+++ glossary-api/netlify/functions/glossary.js
@@ -1,6 +1,23 @@
 const api = {
   glossary: [
     {
+      abbreviation: "CMS",
+      name: "Content Management System",
+      description:
+        "A tool used to manage content, usually allowing custom fields and object types, which handles references, media, and more.",
+    },
+    {
+      abbreviation: "COPE",
+      name: "Create Once, Publish Everywhere",
+      description:
+        "A term used to describe the experience of only authoring a piece of content once with the ability to publish it on any medium without modifying it.",
+    },
+    {
+      abbreviation: "GJS",
+      name: "GatsbyJS",
+      description: "The name of a popular site generator",
+    },
+    {
       abbreviation: "JAM",
       name: "Just Another Mountain",
       description:
@@ -10,6 +27,12 @@ const api = {
       abbreviation: "MAATT",
       name: "Much Ado About Time Travel",
       description: "A codename for a secret project dealing with time travel",
+    },
+    {
+      abbreviation: "WYSIWYG",
+      name: "What You See Is What You Get",
+      description:
+        "A term used in the context of visual editors to mean that what you see is what your users see",
     },
   ],
 };
