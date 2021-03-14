52108967481264bd8d0870d4b0142efc495bda07
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-13 23:34:42 -0600

    Add author meta tag

--- globomantics-blog-theme/blog-theme/gatsby-ssr.js
+++ globomantics-blog-theme/blog-theme/gatsby-ssr.js
@@ -1,6 +1,10 @@
-const React = require('react');
+const React = require("react");
 const Layout = require("./src/components/layout").default;
 
 exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
+};
+
+exports.onRenderBody = ({ setHeadComponents }, { authorId }) => {
+  setHeadComponents([<meta key="author" name="author" content={authorId} />]);
 };
