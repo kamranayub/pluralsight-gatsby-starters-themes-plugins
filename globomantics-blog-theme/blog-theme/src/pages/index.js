c18dab759cb4b10419430eabbc6f3585ef2fbd7d
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-13 23:35:15 -0600

    index

--- globomantics-blog-theme/blog-theme/src/pages/index.js
+++ globomantics-blog-theme/blog-theme/src/pages/index.js
@@ -1,46 +1,43 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import Head from '../components/head'
-import Hero from '../components/hero'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import React from "react";
+import { graphql } from "gatsby";
+import get from "lodash/get";
+import Head from "../components/head";
+import Hero from "../components/hero";
+import ArticlePreview from "../components/article-preview";
 
 class RootIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const posts = get(this, "props.data.allContentfulBlogPost.edges");
+    const [author] = get(this, "props.data.allContentfulPerson.edges");
 
     return (
-      <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
-          <Head title={siteTitle} />
-          <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+      <div style={{ background: "#fff" }}>
+        <Head title={siteTitle} />
+        <Hero data={author.node} />
+        <div className="wrapper">
+          <h2 className="section-headline">Recent articles</h2>
+          <ul className="article-list">
+            {posts.map(({ node }) => {
+              return (
+                <li key={node.slug}>
+                  <ArticlePreview article={node} />
+                </li>
+              );
+            })}
+          </ul>
         </div>
-      </Layout>
-    )
+      </div>
+    );
   }
 }
 
-export default RootIndex
+export default RootIndex;
 
 export const pageQuery = graphql`
   query HomeQuery($authorId: String) {
     allContentfulBlogPost(
-      filter: { author: { contentful_id: { eq: $authorId } } }, 
+      filter: { author: { contentful_id: { eq: $authorId } } }
       sort: { fields: [publishDate], order: DESC }
     ) {
       edges {
@@ -62,9 +59,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: $authorId } }
-    ) {
+    allContentfulPerson(filter: { contentful_id: { eq: $authorId } }) {
       edges {
         node {
           name
@@ -73,11 +68,7 @@ export const pageQuery = graphql`
           }
           title
           heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-            ) {
+            fluid(maxWidth: 1180, maxHeight: 480, resizingBehavior: PAD) {
               ...GatsbyContentfulFluid
             }
           }
@@ -85,4 +76,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
