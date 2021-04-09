cc4f3851715f157fe311e553675edc81928ed795
Kamran Ayub <kamran.ayub@gmail.com>
2021-04-08 22:42:41 -0500

    fixup package.json

--- globomantics-blog-theme/example/gatsby-config.js
+++ globomantics-blog-theme/example/gatsby-config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: [{ resolve: `@kamranayub/globomantics-blog-theme`, options: {} }],
+  plugins: [{ resolve: `@kamranayub/gatsby-theme-globomantics-blog`, options: {} }],
 };
