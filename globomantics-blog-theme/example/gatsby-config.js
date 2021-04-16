348fd6dad1b053089192f6360ed45b5edf4e1cb3
Kamran Ayub <kamran.ayub@gmail.com>
2021-04-16 01:00:09 -0500

    Revert back to converted theme commit

--- globomantics-blog-theme/example/gatsby-config.js
+++ globomantics-blog-theme/example/gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: [{ resolve: `@kamranayub/gatsby-theme-globomantics-blog`, options: {} }],
+  plugins: [
+    {
+      resolve: `@kamranayub/gatsby-theme-globomantics-blog`,
+      options: {
+        title: "Example Themed Blog",
+        authorId: "15jwOBqpxqSAOy2eOO4S0m", // YL4ATa1RwAn9uZqw29KU7
+      },
+    },
+  ],
 };
